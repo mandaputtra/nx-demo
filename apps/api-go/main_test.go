3923fa1ae0e55ff46a48be9ba5fefd522e83bbbb
Manda Putra <mandaputra8@gmail.com>
2023-06-17 08:33:40 +0700

    feat: golang test

--- apps/api-go/main_test.go
+++ apps/api-go/main_test.go
@@ -3,7 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -11,20 +11,37 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func performRequest(r http.Handler, method, path string, body io.Reader) *httptest.ResponseRecorder {
+   req, _ := http.NewRequest(method, path, body)
+   w := httptest.NewRecorder()
+   r.ServeHTTP(w, req)
+   return w
+}
+
 func TestAddRoute(t *testing.T) {
 	router := setupRouter()
 
-  mcPostBody := map[string]interface{}{
+  itemPostBody := map[string]interface{}{
     "title": "Title would be great",
-    "year": 1001,
+    "year": 1992,
   }
-  body, _ := json.Marshal(mcPostBody)
+  body, _ := json.Marshal(itemPostBody)
+  wAdd := performRequest(router, "POST", "/", bytes.NewReader(body))
 
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("POST", "/", bytes.NewReader(body))
-	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, wAdd.Code)
 
-  fmt.Println(w.Body.String())
+  wGet := performRequest(router, "GET", "/", nil)
+  assert.Equal(t, 200, wGet.Code)
+}
 
-	assert.Equal(t, 200, w.Code)
+func TestJSONSchemaFail(t *testing.T) {
+	router := setupRouter()
+
+  itemPostBody := map[string]interface{}{
+    "title": "Title would be great",
+    "year": 10,
+  }
+  body, _ := json.Marshal(itemPostBody)
+  w := performRequest(router, "POST", "/", bytes.NewReader(body))
+	assert.Equal(t, 400, w.Code)
 }
